19fad3c035b5c7ebb2626c3b5c95fd1c7d18acd8
Monique <moniquerz@Moniques-MacBook-Pro.local>
2018-08-09 16:47:42 -0400

    finishes get all users, get user by name, get album by title routes

--- functions/index.js
+++ functions/index.js
@@ -1,21 +1,68 @@
 const functions = require('firebase-functions');
 const express = require('express');
+const asyncHandler = require('express-async-handler')
+const request =  require('request');
+const { db } = require('../src/firebase');
+const usersCollection = db.collection('users');
 
-/* Express */
-const app1 = express();
-app1.get('/home', (request, response) => {
-  console.log('Hit this route');
-  response.send('Home!!!!');
-});
+/*Express*/
 
-app1.get('/users', (request, response) => {
-  console.log('Hit this route');
-  response.send('Users!!!!');
-});
+const app = express();
 
-app1.get('/songs', (request, response) => {
-  console.log('Hit this route');
-  response.send('Songs!!!!!!');
-});
 
-exports.api = functions.https.onRequest(app1);
+//GET all users
+app.get('/users', asyncHandler(async (req, res, next) => {
+  console.log('getting users')
+  const users = [];
+  const data = await usersCollection.get();
+  data.forEach(doc => users.push(doc.data()));
+  if(data === undefined){
+    res.send('No data found')
+  }else{
+    res.send(users)
+  }
+  }));
+
+
+//GET user by name
+app.get('/user', asyncHandler(async (req, res, next) => {
+  console.log('getting user')
+  // get user by name
+  const user = await usersCollection.doc('user1').get();
+  if (user === undefined) {
+    res.send('No data found')
+  } else {
+    res.send(user.data())
+  }
+}));
+
+
+//GET Album by tittle
+
+// "https://api.spotify.com/v1/albums/0sNOF9WDwhWunNAHPD3Baj?market=US" - H "Authorization: Bearer BQD42fP09rP2t4LeRD9WYT9AgEZdCsyv7zHCptRTtXluC3uJQQ8acpy1tvJo3WxHKmHqcF6k4_eyC-t9ItD1z8d-AxPuviNeFwj2AQmesj2PbKDxn1XmDLf9fTagMyP7_1JLb_d-7HPs8aVb"
+
+
+app.get('/album/', asyncHandler(async(req,res,next)=>{
+  console.log('getting album by title');
+  //get user by name
+  const user = await usersCollection.doc('user1').get();
+  const userRefreshToken = user.data().refresh_token;
+  const albumId = '0sNOF9WDwhWunNAHPD3Baj'
+
+  const options = { url: `https://api.spotify.com/v1/albums/${albumId}?market=US`, 
+  headers: { Authorization: 'Bearer BQD42fP09rP2t4LeRD9WYT9AgEZdCsyv7zHCptRTtXluC3uJQQ8acpy1tvJo3WxHKmHqcF6k4_eyC-t9ItD1z8d-AxPuviNeFwj2AQmesj2PbKDxn1XmDLf9fTagMyP7_1JLb_d-7HPs8aVb' } };
+
+
+  const album = await request(options, (error,response,body) => {
+    if(!error && response.statusCode == 200){
+      console.log('album', body)
+      res.send(JSON.parse(body))
+    }else{
+      res.send('NO album found')
+    }
+  } )
+}));
+
+
+
+exports.api = functions.https.onRequest(app);
