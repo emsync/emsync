7098c9abc82a50146de3e9983628caea9a3b70a8
Rick Terry <fdterr@gmail.com>
2018-08-09 17:25:38 -0400

    preliminary navbar

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,23 @@
+const path = require('path');
 const functions = require('firebase-functions');
 const express = require('express');
 
 /* Express */
 const app1 = express();
+
+// app1.use('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '..', 'public/index.html'));
+// });
+
+// app1.get('*', (req, res, next) => {
+//   console.log('hitting catch all route');
+//   // res.send('hitting catch all route');
+//   next();
+// });
+
 app1.get('/home', (request, response) => {
   console.log('Hit this route');
-  response.send('Home!!!!');
+  response.send('Home2!!!!');
 });
 
 app1.get('/users', (request, response) => {
